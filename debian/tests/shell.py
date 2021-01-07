c7b44788c1ee2328490dccd63c8a76c341cca58f
dann frazier <dannf@debian.org>
2021-01-06 19:02:07 -0700

    autopkgtest: Drop 'atexit' usage
    Avoid needing to use 'atexit' callbacks to cleanup uefi variable flash
    images by implementing a PflashParams object that cleans itself up when
    deleted.

--- debian/tests/shell.py
+++ debian/tests/shell.py
@@ -16,17 +16,11 @@
 # You should have received a copy of the GNU General Public License along with
 # this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import atexit
-import os
 import pexpect
 import shutil
 import sys
 import tempfile
 import unittest
-
-
-def cleanup_file(f):
-    os.unlink(f)
 
 
 class BootToShellTest(unittest.TestCase):
@@ -48,17 +42,23 @@ class BootToShellTest(unittest.TestCase):
         '-device', 'virtio-serial-device',
     ]
 
-    def gen_pflash_params(self, code_path, vars_template_path):
-        (vars_file, vars_path) = tempfile.mkstemp()
-        shutil.copy(vars_template_path, vars_path)
-        atexit.register(cleanup_file, vars_path)
+    class PflashParams:
+        def __init__(self, code_path, vars_template_path):
+            with tempfile.NamedTemporaryFile(delete=False) as varfile:
+                self.varfile_path = varfile.name
+                with open(vars_template_path, 'rb') as template:
+                    shutil.copyfileobj(template, varfile)
+                self.params = [
+                    '-drive',
+                    'file=%s,if=pflash,format=raw,unit=0,readonly=on' %
+                    (code_path),
+                    '-drive',
+                    'file=%s,if=pflash,format=raw,unit=1,readonly=off' %
+                    (varfile.name)
+                ]
 
-        return [
-            '-drive',
-            'file=%s,if=pflash,format=raw,unit=0,readonly=on' % (code_path),
-            '-drive',
-            'file=%s,if=pflash,format=raw,unit=1,readonly=off' % (vars_path),
-        ]
+        def __del__(self):
+            os.unlink(self.varfile_path)
 
     def run_cmd_check_shell(self, cmd):
         child = pexpect.spawn(' '.join(cmd))
@@ -88,10 +88,11 @@ class BootToShellTest(unittest.TestCase):
             'qemu-system-aarch64',
             '-cpu', 'cortex-a57',
         ] + self.Qemu_Common_Params + self.Qemu_Qemu_Efi_Common_Params
-        cmd = cmd + self.gen_pflash_params(
+        pflash = self.PflashParams(
             '/usr/share/AAVMF/AAVMF_CODE.fd',
             '/usr/share/AAVMF/AAVMF_VARS.fd',
         )
+        cmd = cmd + pflash.params
         self.run_cmd_check_shell(cmd)
 
     def test_aavmf32(self):
@@ -99,10 +100,11 @@ class BootToShellTest(unittest.TestCase):
             'qemu-system-aarch64',
             '-cpu', 'cortex-a15',
         ] + self.Qemu_Common_Params + self.Qemu_Qemu_Efi_Common_Params
-        cmd = cmd + self.gen_pflash_params(
+        pflash = self.PflashParams(
             '/usr/share/AAVMF/AAVMF32_CODE.fd',
             '/usr/share/AAVMF/AAVMF32_VARS.fd',
         )
+        cmd = cmd + pflash.params
         self.run_cmd_check_shell(cmd)
 
     def test_ovmf_pc(self):
@@ -110,10 +112,11 @@ class BootToShellTest(unittest.TestCase):
             'qemu-system-x86_64',
             '-machine', 'pc,accel=tcg'
         ] + self.Qemu_Common_Params + self.Qemu_Ovmf_Common_Params
-        cmd = cmd + self.gen_pflash_params(
+        pflash = self.PflashParams(
             '/usr/share/OVMF/OVMF_CODE.fd',
             '/usr/share/OVMF/OVMF_VARS.fd',
         )
+        cmd = cmd + pflash.params
         self.run_cmd_check_shell(cmd)
 
     def test_ovmf_q35(self):
@@ -121,10 +124,11 @@ class BootToShellTest(unittest.TestCase):
             'qemu-system-x86_64',
             '-machine', 'q35,accel=tcg'
         ] + self.Qemu_Common_Params + self.Qemu_Ovmf_Common_Params
-        cmd = cmd + self.gen_pflash_params(
+        pflash = self.PflashParams(
             '/usr/share/OVMF/OVMF_CODE.fd',
             '/usr/share/OVMF/OVMF_VARS.fd',
         )
+        cmd = cmd + pflash.params
         self.run_cmd_check_shell(cmd)
 
     def test_ovmf_secboot(self):
@@ -133,10 +137,11 @@ class BootToShellTest(unittest.TestCase):
             '-machine', 'q35,accel=tcg',
             '-global', 'ICH9-LPC.disable_s3=1',
         ] + self.Qemu_Common_Params + self.Qemu_Ovmf_Common_Params
-        cmd = cmd + self.gen_pflash_params(
+        pflash = self.PflashParams(
             '/usr/share/OVMF/OVMF_CODE.secboot.fd',
             '/usr/share/OVMF/OVMF_VARS.fd',
         )
+        cmd = cmd + pflash.params
         self.run_cmd_check_shell(cmd)
 
     def test_ovmf_ms(self):
@@ -145,10 +150,11 @@ class BootToShellTest(unittest.TestCase):
             '-machine', 'q35,accel=tcg',
             '-global', 'ICH9-LPC.disable_s3=1',
         ] + self.Qemu_Common_Params + self.Qemu_Ovmf_Common_Params
-        cmd = cmd + self.gen_pflash_params(
+        pflash = self.PflashParams(
             '/usr/share/OVMF/OVMF_CODE.ms.fd',
             '/usr/share/OVMF/OVMF_VARS.ms.fd',
         )
+        cmd = cmd + pflash.params
         self.run_cmd_check_shell(cmd)
 
     def test_ovmf_4m(self):
@@ -156,10 +162,11 @@ class BootToShellTest(unittest.TestCase):
             'qemu-system-x86_64',
             '-machine', 'q35,accel=tcg'
         ] + self.Qemu_Common_Params + self.Qemu_Ovmf_Common_Params
-        cmd = cmd + self.gen_pflash_params(
+        pflash = self.PflashParams(
             '/usr/share/OVMF/OVMF_CODE_4M.fd',
             '/usr/share/OVMF/OVMF_VARS_4M.fd',
         )
+        cmd = cmd + pflash.params
         self.run_cmd_check_shell(cmd)
 
     def test_ovmf_4m_secboot(self):
@@ -167,10 +174,11 @@ class BootToShellTest(unittest.TestCase):
             'qemu-system-x86_64',
             '-machine', 'q35,accel=tcg',
         ] + self.Qemu_Common_Params + self.Qemu_Ovmf_Common_Params
-        cmd = cmd + self.gen_pflash_params(
+        pflash = self.PflashParams(
             '/usr/share/OVMF/OVMF_CODE_4M.secboot.fd',
             '/usr/share/OVMF/OVMF_VARS_4M.fd',
         )
+        cmd = cmd + pflash.params
         self.run_cmd_check_shell(cmd)
 
     def test_ovmf_4m_ms(self):
@@ -178,10 +186,11 @@ class BootToShellTest(unittest.TestCase):
             'qemu-system-x86_64',
             '-machine', 'q35,accel=tcg',
         ] + self.Qemu_Common_Params + self.Qemu_Ovmf_Common_Params
-        cmd = cmd + self.gen_pflash_params(
+        pflash = self.PflashParams(
             '/usr/share/OVMF/OVMF_CODE_4M.ms.fd',
             '/usr/share/OVMF/OVMF_VARS_4M.ms.fd',
         )
+        cmd = cmd + pflash.params
         self.run_cmd_check_shell(cmd)
 
     def test_ovmf32_4m_secboot(self):
@@ -189,10 +198,11 @@ class BootToShellTest(unittest.TestCase):
             'qemu-system-i386',
             '-machine', 'q35,accel=tcg'
         ] + self.Qemu_Common_Params + self.Qemu_Ovmf_Common_Params
-        cmd = cmd + self.gen_pflash_params(
+        pflash = self.PflashParams(
             '/usr/share/OVMF/OVMF32_CODE_4M.secboot.fd',
             '/usr/share/OVMF/OVMF32_VARS_4M.fd',
         )
+        cmd = cmd + pflash.params
         self.run_cmd_check_shell(cmd)
 
 
